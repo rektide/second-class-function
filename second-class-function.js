b32d9c625322dbcb87f641e9bc27f06edeaf1645
rektide <rektide@voodoowarez.com>
2014-06-07 02:05:14 -0400

    Build an explicit context.

--- second-class-function.js
+++ second-class-function.js
@@ -1,10 +1,44 @@
+var pipeline2= require("./pipeline2")
+
 module.exports = makeSecondClassFunction
 
+function _results(ctx){
+	return ctx.results
+}
+
+function _fail(ctx){
+	return ctx.error || new Error("SecondClassComputation error")
+}
+
+/**
+* @factory
+*/
 function makeSecondClassFunction() {
-	var chain= []
-	function secondFunction(){
-		return chain2(chain, this, arguments)
+	// build chain from any passed in functions or promised-functions.
+	var chain= Array.prototype.slice(arguments, 0)
+
+	/**
+	* SecondClassFunction
+	* @class
+	*/
+	function SecondClassFunction(){
+		var ctx = {arguments:arguments}
+		return pipeline(chain, this, ctx).then(_results, _fail)
 	}
-	secondFunction.chain= chain
+	/**
+	* The underlying command chain
+	* @memberOf SecondClassFunction
+	*/
+	SecondClassFunction.chain= chain
+
+	/**
+	* apply, returning the continuation, rather than the continuation's results.
+	* @memberOf SecondClassFunction
+	*/
+	function applyRaw(self, args){
+		return chain(chain, this, {arguments:args})
+	}
+	SecondClassFunction.applyRaw = applyRaw
+
 	return secondFunction
 }
